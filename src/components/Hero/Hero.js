b69e9aa5e2d810731a829880a38bbf0ecce4b4e1
Jacek Kącki <jkacki@gmail.com>
2019-11-29 11:43:59 +0100

    change hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import styles from './Hero.scss';
 const Hero = () => (
   <header className={styles.component}>
     <h2 className={styles.title}>Things to do</h2>
-    <img src={'./images/space.png'} className={styles.image} alt={'Space'}/>
+    <img src={'../../src/images/space.png'} alt={'Space'} className={styles.image}/>
   </header>
 );
 
